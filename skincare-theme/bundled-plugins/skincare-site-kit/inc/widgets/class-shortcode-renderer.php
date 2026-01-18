39123784a819cca5900ece0cbf17c025c8efdb49
isabellawellnessco-creator <isabellawellness.co@gmail.com>
2026-01-18 17:11:42 -0500

    Seed shortcode widget settings data

--- skincare-theme/bundled-plugins/skincare-site-kit/inc/widgets/class-shortcode-renderer.php
+++ skincare-theme/bundled-plugins/skincare-site-kit/inc/widgets/class-shortcode-renderer.php
@@ -7,6 +7,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 abstract class Shortcode_Renderer extends \Elementor\Widget_Base {
 	public function render_shortcode( $settings = [] ) {
+		if ( ! is_array( $settings ) ) {
+			$settings = [];
+		}
+
+		if ( method_exists( $this, 'set_data' ) ) {
+			$this->set_data( 'settings', $settings );
+		}
+
 		if ( method_exists( $this, 'set_settings' ) ) {
 			$this->set_settings( $settings );
 		}
